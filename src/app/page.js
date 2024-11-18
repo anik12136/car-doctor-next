77dd8e64b5fd5199a2c76ed10bd370fc775efd62
anik12136 <anikbiswas431@gmail.com>
2024-11-18 14:07:35 +0600

    Basic route setup and config css

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h2>car-doctor</h2>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
